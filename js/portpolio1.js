4ad435689a4f296d6a4afd56d48f765e854858f4
sihyung ahn <aldnj369@gmail.com>
2021-02-16 06:33:24 +0000

    df

--- js/portpolio1.js
+++ js/portpolio1.js
@@ -16,8 +16,8 @@ menu.addEventListener('click', mobileMenu);
 const highlightMenu = () => {
 	const elem = document.querySelector('.highlight');
 	const homeMenu = document.querySelector('#home-page');
-	const aboutMenu = document.querySelector('#about-page');
-	const servicesMenu = document.querySelector('#services-page');
+	const aboutMenu = document.querySelector('#about');
+	const servicesMenu = document.querySelector('#profile');
 	let scrollPos = window.scrollY;
 	// console.log(scrollPos);
 
